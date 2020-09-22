f7d6e2c470334fcedc98f4e34584c044692357bf
Bard69 <k2942318217@163.com>
2020-09-22 18:54:36 +0800

    first commit

---
+++ main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	_ "BeegoWork/routers"
+	"github.com/astaxie/beego"
+)
+
+func main() {
+	beego.Run()
+}
+
---
+++ routers/router.go
@@ -0,0 +1,10 @@
+package routers
+
+import (
+	"BeegoWork/controllers"
+	"github.com/astaxie/beego"
+)
+
+func init() {
+    beego.Router("/", &controllers.MainController{})
+}
