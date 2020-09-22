689f3915f666293f3676ff972e284d3d137e063a
Bard69 <k2942318217@163.com>
2020-09-22 19:23:04 +0800

    修改模板文件的网址和联系人

--- controllers/default.go
+++ controllers/default.go
@@ -9,7 +9,7 @@ type MainController struct {
 }
 
 func (c *MainController) Get() {
-	c.Data["Website"] = "beego.me"
-	c.Data["Email"] = "astaxie@gmail.com"
+	c.Data["Website"] = "https://www.baidu.com"
+	c.Data["Email"] = "k2942318217@163.com"
 	c.TplName = "index.tpl"
 }
